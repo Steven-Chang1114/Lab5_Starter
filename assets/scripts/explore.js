cfe32143be0d7a097e6b9a8eea9dd921ce46815b
chrisfan0831 <chrisfan0831@gmail.com>
2021-10-22 14:27:31 -0700

    add voice dropdown list

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,33 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const button = document.getElementsByTagName("button")
 }
+
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
+  var voices = speechSynthesis.getVoices();
+  for(var i = 0; i < voices.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+button.addEventListener("click", () => {
+  
+})
+
