659a73833ac8d7c0f429cda6837a4e8058b4ae67
Steven Chang <1632460272@qq.com>
2021-10-22 17:12:23 -0700

    Finish the lab

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,17 +5,47 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
   const button = document.getElementsByTagName("button")[0]
+  const image = document.getElementsByTagName("img")[0];
   const text = document.getElementsByTagName("textarea")[0]
   const synth = window.speechSynthesis;
   const voiceSelect = document.getElementById("voice-select")
-  var voices
+  let voices;
 
+  populateVoiceList();
 
-  function populateVoiceList() {
+  // synth.speaking.addEventListener("change", ()=> {
+  //   console.log("Changed")
+  // })
+
+  button.addEventListener("click", () => {
+    voices = speechSynthesis.getVoices();
+    var utterThis = new SpeechSynthesisUtterance(text.value);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(let i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+
+    if (synth.speaking) {
+      image.src = `assets/images/smiling-open.png`
+    } 
+
+    setTimeout(() => {
+      image.src = `assets/images/smiling.png`
+    }, 1000)
+
+    text.blur();
+  })
+}
+
+  function populateVoiceList(voices) {
     if(typeof speechSynthesis === 'undefined') {
       return;
     }
     voices = speechSynthesis.getVoices();
+
     for(var i = 0; i < voices.length; i++) {
       var option = document.createElement('option');
       option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
@@ -28,23 +58,10 @@ function init() {
       document.getElementById("voice-select").appendChild(option);
     }
   }
-
-  populateVoiceList();
   if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  button.addEventListener("click", () => {
-    var utterThis = new SpeechSynthesisUtterance(text.value);
-    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-    for(let i = 0; i < voices.length ; i++) {
-      if(voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
-      }
-    }
-    synth.speak(utterThis);
-    text.blur();
-  })
 
-}
+
 
